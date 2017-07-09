03cb65cb8e172cf2ddf235ad907d7ce12d672dcd
Shai Cantor <shai@sealights.io>
2017-07-09 20:26:48 +0300

    modified files 1 for test priority

--- src/main/java/com/kuhniverse/web/CaptchaController.java
+++ src/main/java/com/kuhniverse/web/CaptchaController.java
@@ -44,7 +44,7 @@ public class CaptchaController {
     @RequestMapping(value = "/start/{howMany}", method = RequestMethod.GET)
     @ResponseBody
     public CaptchaFrontEndData start(@PathVariable int howMany) {
-        int nadav = 0;
+        int nadav = 1;
         return captchaSession.start(howMany);
     }
 
